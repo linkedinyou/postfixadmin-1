778a4b985bf0d9ce6ed7038a3db64cf193a49738
Juliano Martinez <juliano@falaserio.org>
2011-03-07 17:14:38 -0300

    Added auto response app

---
+++ autoresponse/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib import admin
+from postfixadmin.users.models import User
+
+class Response(models.Model):
+    name = models.CharField(max_length=200)
+    enabled = models.BooleanField(default=False)
+    user = models.OneToOneField(User, related_name='User')
+
+    def __unicode__(self):
+        return self.name
